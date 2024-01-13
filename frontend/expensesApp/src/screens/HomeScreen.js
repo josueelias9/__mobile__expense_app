8ac4dfd629693ba8ffeb3e93e825d687d1de249e
Josue Huaman <josueeliashuaman@gmail.com>
2024-01-12 21:53:21 -0500

    removing unecesary code

--- frontend/expensesApp/src/screens/HomeScreen.js
+++ frontend/expensesApp/src/screens/HomeScreen.js
@@ -1,27 +1,6 @@
-import React, { useEffect, useState } from 'react'
-
 import { Text, View } from 'react-native'
 import MyForm from '../components/MyForm'
 function HomeScreen() {
-  const [isLoading, setLoading] = useState(true)
-  const [data, setData] = useState([])
-
-  const getMovies = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/api/expenses')
-      const json = await response.json()
-      console.log(json)
-      // setData(json.movies);
-    } catch (error) {
-      console.error(error)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    getMovies()
-  }, [])
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
