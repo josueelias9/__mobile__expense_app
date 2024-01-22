ad18f729480b321ac4614d5b761b02fac7fcf82d
Josue Huaman <josueeliashuaman@gmail.com>
2024-01-22 11:35:05 -0500

    adding static icon

--- frontend/expensesApp/App.js
+++ frontend/expensesApp/App.js
@@ -4,6 +4,8 @@ import HomeScreen from './src/screens/HomeScreen'
 import Detail from './src/screens/Detail'
 import Dashboard from './src/screens/Dashboard'
 import AddScreen from './src/screens/AddScreen'
+import { StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const Tab = createMaterialTopTabNavigator()
 
@@ -22,6 +24,17 @@ export default function App() {
         <Tab.Screen name='Home' component={HomeScreen} />
         <Tab.Screen name='Detail' component={Detail} />
       </Tab.Navigator>
+      <Icon name='md-star' size={30} color='#000' style={styles.icon} />
     </NavigationContainer>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  icon: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10
+  }
+})
