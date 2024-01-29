732ed6d365cc9be2354a4bd639f5eeac286545f9
Josue Huaman <josueeliashuaman@gmail.com>
2024-01-28 19:15:27 -0500

    adding POST verb to BaseModal component

--- frontend/expensesApp/src/baseComponents/BaseModal.js
+++ frontend/expensesApp/src/baseComponents/BaseModal.js
@@ -1,11 +1,25 @@
+// https://docs.nativebase.io/input#h3-controlled-input
 //@ts-nocheck
 import React from 'react'
 import { Button, Modal, Stack, FormControl, Input } from 'native-base'
 import { useState } from 'react'
+import { postExpensesFromApiAsync } from '../api/tryFetch'
 
 const Example = ({ labelOne, labelTwo, buttonName }) => {
   const [placement, setPlacement] = useState(undefined)
   const [open, setOpen] = useState(false)
+  const [value1, setValue1] = React.useState({
+    amount:"",
+    shortDescription:""
+  });
+
+  const handleChange = (text,key) => {
+    setValue1({
+      ...value1,
+      [key]: text,
+    })
+    // console.log(value1)
+  };
 
   const openModal = placement => {
     setOpen(true)
@@ -30,11 +44,11 @@ const Example = ({ labelOne, labelTwo, buttonName }) => {
           <Modal.Body>
             <FormControl>
               <FormControl.Label>{labelOne}</FormControl.Label>
-              <Input />
+              <Input value={value1.amount} w="100%" onChangeText={(text)=>handleChange(text,"amount")} placeholder="Value Controlled Input" />            
             </FormControl>
             <FormControl mt='3'>
               <FormControl.Label>{labelTwo}</FormControl.Label>
-              <Input />
+              <Input value={value1.shortDescription} w="100%" onChangeText={(text)=>handleChange(text,"shortDescription")} placeholder="Value Controlled Input" />            
             </FormControl>
           </Modal.Body>
           <Modal.Footer>
@@ -51,6 +65,7 @@ const Example = ({ labelOne, labelTwo, buttonName }) => {
               <Button
                 onPress={() => {
                   setOpen(false)
+                  postExpensesFromApiAsync(value1)
                 }}
               >
                 Save
